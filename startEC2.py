3be7d378607c5c7f284dea23c7f054febca674e5
Adriano Canofre <adriano.canofre@tvglobo.com.br>
2019-04-10 18:30:55 -0300

    fix in start

--- startEC2.py
+++ startEC2.py
@@ -1,10 +1,11 @@
 import boto3
-region = 'us-east-1'
+
+ec2 = boto3.resource('ec2')
 
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name=region)
+    
     filters = [{
-            'Name': 'tag:AutoOff',
+            'Name': 'tag:AutoStartStop',
             'Values': ['True']
         },
         {
@@ -12,11 +13,12 @@ def lambda_handler(event, context):
             'Values': ['stopped']
         }
     ]
+
     instances = ec2.instances.filter(Filters=filters)
 
     start_ec2 = [instance.id for instance in instances]
+
     if len(start_ec2) > 0:
-        start = ec2.instances.filter(InstanceIds=instances).start()
-	    print start
+        shuttingDown = ec2.instances.filter(InstanceIds=start_ec2).start()
     else:
         print "No instances to be started found"
--- stopEC2.py
+++ stopEC2.py
@@ -1,6 +1,5 @@
 import boto3
 
-#define the connection
 ec2 = boto3.resource('ec2')
 
 def lambda_handler(event, context):
