ccedc0c4eca424de5516eca03425fbf245d515af
Adriano Canofre <adriano.canofre@tvglobo.com.br>
2019-04-10 18:21:17 -0300

    Fix in stop function

--- stopEC2.py
+++ stopEC2.py
@@ -1,10 +1,12 @@
 import boto3
-region = 'us-east-1'
+
+#define the connection
+ec2 = boto3.resource('ec2')
 
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name=region)
+
     filters = [{
-            'Name': 'tag:AutoOff',
+            'Name': 'tag:AutoStartStop',
             'Values': ['True']
         },
         {
@@ -12,11 +14,12 @@ def lambda_handler(event, context):
             'Values': ['running']
         }
     ]
+
     instances = ec2.instances.filter(Filters=filters)
 
-    stop_ec2 = [instance.id for instance in instances]
-    if len(stop_ec2) > 0:
-        stop = ec2.instances.filter(InstanceIds=instances).stop()
-	    print stop
+    RunningInstances = [instance.id for instance in instances]
+
+    if len(RunningInstances) > 0:
+        shuttingDown = ec2.instances.filter(InstanceIds=RunningInstances).stop()
     else:
-        print "No instances to be stopped found"
+        print "Nothing to see here"
